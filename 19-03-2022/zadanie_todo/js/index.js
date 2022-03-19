b531e752d1ff17d789935265a2243b07fae69663
robert.gontarski <gontarskir@main.viagoldltd.com>
2022-03-19 17:02:28 +0100

    install eslint + prettier

--- 19-03-2022/zadanie_todo/js/index.js
+++ 19-03-2022/zadanie_todo/js/index.js
@@ -1,95 +1,94 @@
-if (localStorage.getItem('tasks') === null) {
-    localStorage.setItem('tasks', JSON.stringify({}));
+if (localStorage.getItem("tasks") === null) {
+  localStorage.setItem("tasks", JSON.stringify({}));
 }
 
-const getTasks = () => {
-    return JSON.parse(localStorage.getItem('tasks'));
-}
+const getTasks = () => JSON.parse(localStorage.getItem("tasks"));
 
 const setTasks = (tasks) => {
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-}
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+};
 
 const initTasks = () => {
-    const tasks = getTasks();
+  const tasks = getTasks();
 
-    if (Object.keys(tasks).length) {
-        for (const property in tasks) {
-            const isDeleted = tasks[property].isDeleted ? 'deleted' : '';
-            const isChecked = tasks[property].isChecked;
+  if (Object.keys(tasks).length) {
+    for (const property in tasks) {
+      const isDeleted = tasks[property].isDeleted ? "deleted" : "";
+      const { isChecked } = tasks[property];
 
-            const html = `<div class="task ${isDeleted}" id="${property}">
-                <span class="check-box ${isChecked ? 'checked' : ''}" data-check="${property}"></span>
-                <p class="task-text ${isChecked ? 'checked' : ''}">${tasks[property].name}</p>
+      const html = `<div class="task ${isDeleted}" id="${property}">
+                <span class="check-box ${
+                  isChecked ? "checked" : ""
+                }" data-check="${property}"></span>
+                <p class="task-text ${isChecked ? "checked" : ""}">${
+        tasks[property].name
+      }</p>
                 <span class="delete delete-task" data-delete="${property}">remove</span>
-            </div>`
+            </div>`;
 
-            document.querySelector('.tasks').innerHTML += html;
-        }
+      document.querySelector(".tasks").innerHTML += html;
     }
-}
+  }
+};
 
 const formAction = () => {
-    document.querySelector('#todoForm').addEventListener('submit', (e) => {
-        e.preventDefault();
-        const input = document.querySelector('#taskName');
-        const inputInfo = document.querySelector(`#${input.id}Info`);
+  document.querySelector("#todoForm").addEventListener("submit", (e) => {
+    e.preventDefault();
+    const input = document.querySelector("#taskName");
+    const inputInfo = document.querySelector(`#${input.id}Info`);
 
-        if (input.value.length < 2) {
-            input.classList.add('show-error');
-            inputInfo.classList.add('show-error');
-            return;
-        } else {
-            input.classList.remove('show-error');
-            inputInfo.classList.remove('show-error');
-        }
+    if (input.value.length < 2) {
+      input.classList.add("show-error");
+      inputInfo.classList.add("show-error");
+      return;
+    }
+    input.classList.remove("show-error");
+    inputInfo.classList.remove("show-error");
 
-        const newTask = {
-            name: input.value,
-            isChecked: false,
-            isDeleted: false
-        }
+    const newTask = {
+      name: input.value,
+      isChecked: false,
+      isDeleted: false,
+    };
 
-        const tasks = getTasks();
-        const itemIndex = Object.keys(tasks).length;
+    const tasks = getTasks();
+    const itemIndex = Object.keys(tasks).length;
 
-        tasks[`task${itemIndex}`] = newTask;
-        setTasks(tasks);
+    tasks[`task${itemIndex}`] = newTask;
+    setTasks(tasks);
 
-        const html = `<div class="task" id="task${itemIndex}">
+    const html = `<div class="task" id="task${itemIndex}">
                 <span class="check-box" data-check="task${itemIndex}"></span>
                 <p class="task-text">${newTask.name}</p>
                 <span class="material-icons-outlined delete delete-task" data-delete="task${itemIndex}">remove</span>
             </div>`;
 
-        document.querySelector('.tasks').innerHTML += html;
-        input.value = null;
-    });
-}
+    document.querySelector(".tasks").innerHTML += html;
+    input.value = null;
+  });
+};
 
 const taskClickHandler = () => {
-    document.querySelector('.tasks').addEventListener('click', (e) => {
-        console.log();
-        if (e.target.dataset.check !== undefined) {
-            const task = e.target.dataset.check;
-            const tasks = JSON.parse(localStorage.getItem('tasks'));
-            tasks[task].isChecked = true;
-            setTasks(tasks);
+  document.querySelector(".tasks").addEventListener("click", (e) => {
+    if (e.target.dataset.check !== undefined) {
+      const task = e.target.dataset.check;
+      const tasks = JSON.parse(localStorage.getItem("tasks"));
+      tasks[task].isChecked = true;
+      setTasks(tasks);
 
-            document.querySelector(`#${task} .task-text`).classList.toggle('checked');
-            e.target.classList.toggle('checked');
-        } else if (e.target.dataset.delete !== undefined) {
-            const task = e.target.dataset.delete;
-            const tasks = getTasks();
+      document.querySelector(`#${task} .task-text`).classList.toggle("checked");
+      e.target.classList.toggle("checked");
+    } else if (e.target.dataset.delete !== undefined) {
+      const task = e.target.dataset.delete;
+      const tasks = getTasks();
 
-            tasks[task].isDeleted = true;
-            setTasks(tasks);
+      tasks[task].isDeleted = true;
+      setTasks(tasks);
 
-            document.querySelector(`#${task}`).classList.toggle('deleted');
-        }
-    })
-}
-
+      document.querySelector(`#${task}`).classList.toggle("deleted");
+    }
+  });
+};
 
 formAction();
 initTasks();
