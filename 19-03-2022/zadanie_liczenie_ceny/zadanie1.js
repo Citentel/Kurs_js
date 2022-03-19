f86a25a51edf957e378d2fd6f645afa06f102b5d
robert.gontarski <gontarskir@main.viagoldltd.com>
2022-03-19 17:05:11 +0100

    install eslint + prettier

--- 19-03-2022/zadanie_liczenie_ceny/zadanie1.js
+++ 19-03-2022/zadanie_liczenie_ceny/zadanie1.js
@@ -1,7 +1,7 @@
 const products = [
-    { name: "Bananas", pricePerUnit: 1.49, quantity: 2 },
-    { name: "Bread", pricePerUnit: 3.29, quantity: 1 },
-    { name: "Chocolate", pricePerUnit: 5, quantity: 2 },
+    {name: "Bananas", pricePerUnit: 1.49, quantity: 2},
+    {name: "Bread", pricePerUnit: 3.29, quantity: 1},
+    {name: "Chocolate", pricePerUnit: 5, quantity: 2},
 ];
 
 function sumItems(products) {
--- 19-03-2022/zadanie_todo/js/index.js
+++ 19-03-2022/zadanie_todo/js/index.js
@@ -1,11 +1,11 @@
-if (localStorage.getItem("tasks") === null) {
-  localStorage.setItem("tasks", JSON.stringify({}));
+if (localStorage.getItem('tasks') === null) {
+  localStorage.setItem('tasks', JSON.stringify({}));
 }
 
-const getTasks = () => JSON.parse(localStorage.getItem("tasks"));
+const getTasks = () => JSON.parse(localStorage.getItem('tasks'));
 
 const setTasks = (tasks) => {
-  localStorage.setItem("tasks", JSON.stringify(tasks));
+  localStorage.setItem('tasks', JSON.stringify(tasks));
 };
 
 const initTasks = () => {
@@ -13,37 +13,37 @@ const initTasks = () => {
 
   if (Object.keys(tasks).length) {
     for (const property in tasks) {
-      const isDeleted = tasks[property].isDeleted ? "deleted" : "";
+      const isDeleted = tasks[property].isDeleted ? 'deleted' : '';
       const { isChecked } = tasks[property];
 
       const html = `<div class="task ${isDeleted}" id="${property}">
                 <span class="check-box ${
-                  isChecked ? "checked" : ""
-                }" data-check="${property}"></span>
-                <p class="task-text ${isChecked ? "checked" : ""}">${
-        tasks[property].name
-      }</p>
+  isChecked ? 'checked' : ''
+}" data-check="${property}"></span>
+                <p class="task-text ${isChecked ? 'checked' : ''}">${
+  tasks[property].name
+}</p>
                 <span class="delete delete-task" data-delete="${property}">remove</span>
             </div>`;
 
-      document.querySelector(".tasks").innerHTML += html;
+      document.querySelector('.tasks').innerHTML += html;
     }
   }
 };
 
 const formAction = () => {
-  document.querySelector("#todoForm").addEventListener("submit", (e) => {
+  document.querySelector('#todoForm').addEventListener('submit', (e) => {
     e.preventDefault();
-    const input = document.querySelector("#taskName");
+    const input = document.querySelector('#taskName');
     const inputInfo = document.querySelector(`#${input.id}Info`);
 
     if (input.value.length < 2) {
-      input.classList.add("show-error");
-      inputInfo.classList.add("show-error");
+      input.classList.add('show-error');
+      inputInfo.classList.add('show-error');
       return;
     }
-    input.classList.remove("show-error");
-    inputInfo.classList.remove("show-error");
+    input.classList.remove('show-error');
+    inputInfo.classList.remove('show-error');
 
     const newTask = {
       name: input.value,
@@ -63,21 +63,21 @@ const formAction = () => {
                 <span class="material-icons-outlined delete delete-task" data-delete="task${itemIndex}">remove</span>
             </div>`;
 
-    document.querySelector(".tasks").innerHTML += html;
+    document.querySelector('.tasks').innerHTML += html;
     input.value = null;
   });
 };
 
 const taskClickHandler = () => {
-  document.querySelector(".tasks").addEventListener("click", (e) => {
+  document.querySelector('.tasks').addEventListener('click', (e) => {
     if (e.target.dataset.check !== undefined) {
       const task = e.target.dataset.check;
-      const tasks = JSON.parse(localStorage.getItem("tasks"));
+      const tasks = JSON.parse(localStorage.getItem('tasks'));
       tasks[task].isChecked = true;
       setTasks(tasks);
 
-      document.querySelector(`#${task} .task-text`).classList.toggle("checked");
-      e.target.classList.toggle("checked");
+      document.querySelector(`#${task} .task-text`).classList.toggle('checked');
+      e.target.classList.toggle('checked');
     } else if (e.target.dataset.delete !== undefined) {
       const task = e.target.dataset.delete;
       const tasks = getTasks();
@@ -85,7 +85,7 @@ const taskClickHandler = () => {
       tasks[task].isDeleted = true;
       setTasks(tasks);
 
-      document.querySelector(`#${task}`).classList.toggle("deleted");
+      document.querySelector(`#${task}`).classList.toggle('deleted');
     }
   });
 };
