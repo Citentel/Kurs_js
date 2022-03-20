f9276f4d1b20a3ea148f5ec58bcabff4ff8e1860
robert.gontarski <gontarskir@main.viagoldltd.com>
2022-03-20 09:03:19 +0100

    changes

--- 19-03-2022/zadanie_todo/js/index.js
+++ 19-03-2022/zadanie_todo/js/index.js
@@ -6,63 +6,53 @@ const taskElement = (task) => `<div class="task ${task.isDeleted}" id="${task.id
                 <span class="delete delete-task" data-action="delete" data-id="${task.id}">remove</span>
             </div>`;
 
-const initTasks = () => {
-  if (tasks.length > 0) {
-    tasks.forEach((task) => {
-      if (!task.isDeleted) {
-        document.querySelector('.tasks').innerHTML += taskElement(task);
-      }
-    });
-  }
-};
-
-const formAction = () => {
-  document.querySelector('#todoForm').addEventListener('submit', (e) => {
-    e.preventDefault();
-    const input = document.querySelector('#taskName');
-    const inputInfo = document.querySelector(`#${input.id}Info`);
-
-    if (input.value.length < 2) {
-      input.classList.add('show-error');
-      inputInfo.classList.add('show-error');
-      return;
+if (tasks.length > 0) {
+  tasks.forEach((task) => {
+    if (!task.isDeleted) {
+      document.querySelector('.tasks').innerHTML += taskElement(task);
     }
-    input.classList.remove('show-error');
-    inputInfo.classList.remove('show-error');
-
-    const newTask = {
-      id: `task_${tasks.length}`,
-      name: input.value,
-      isChecked: false,
-      isDeleted: false,
-    };
-    tasks = [...tasks, newTask];
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-    document.querySelector('.tasks').innerHTML += taskElement(newTask);
-    input.value = null;
   });
-};
+}
 
-const taskClickHandler = () => {
-  document.querySelector('.tasks').addEventListener('click', (e) => {
-    const result = tasks.map((task) => {
-      const loopElement = task;
-      if (e.target.dataset.id === loopElement.id) {
-        if (e.target.dataset.action === 'check') {
-          loopElement.isChecked = true;
-          document.querySelector(`#${loopElement.id} .task-text`).classList.toggle('checked');
-          e.target.classList.toggle('checked');
-        } else if (e.target.dataset.action === 'delete') {
-          loopElement.isDeleted = true;
-          document.querySelector(`#${loopElement.id}`).classList.toggle('deleted');
-        }
+document.querySelector('#todoForm').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const input = document.querySelector('#taskName');
+  const inputInfo = document.querySelector(`#${input.id}Info`);
+
+  if (input.value.length < 2) {
+    input.classList.add('show-error');
+    inputInfo.classList.add('show-error');
+    return;
+  }
+  input.classList.remove('show-error');
+  inputInfo.classList.remove('show-error');
+
+  const newTask = {
+    id: `task_${tasks.length}`,
+    name: input.value,
+    isChecked: false,
+    isDeleted: false,
+  };
+  tasks = [...tasks, newTask];
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  document.querySelector('.tasks').innerHTML += taskElement(newTask);
+  input.value = null;
+});
+
+document.querySelector('.tasks').addEventListener('click', (e) => {
+  const result = tasks.map((task) => {
+    const loopElement = task;
+    if (e.target.dataset.id === loopElement.id) {
+      if (e.target.dataset.action === 'check') {
+        loopElement.isChecked = true;
+        document.querySelector(`#${loopElement.id} .task-text`).classList.toggle('checked');
+        e.target.classList.toggle('checked');
+      } else if (e.target.dataset.action === 'delete') {
+        loopElement.isDeleted = true;
+        document.querySelector(`#${loopElement.id}`).classList.toggle('deleted');
       }
-      return loopElement;
-    });
-    localStorage.setItem('tasks', JSON.stringify(result));
+    }
+    return loopElement;
   });
-};
-
-formAction();
-initTasks();
-taskClickHandler();
+  localStorage.setItem('tasks', JSON.stringify(result));
+});
